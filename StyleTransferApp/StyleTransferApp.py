7fdfd7339aeb3fb1a1bdd6ff97775ef20f58f16a
BlainBahls <98792178+BlainBahls@users.noreply.github.com>
2022-09-29 13:22:53 -0400

    Add project files.

---
+++ StyleTransferApp/StyleTransferApp.py
@@ -0,0 +1,13 @@
+import os
+from hashlib import new
+import tkinter as tk
+window=tk.Tk()
+window.title(" Pythonista Planet Desktop App ")
+window.geometry("600x400")
+newlabel = tk.Label(text = " Visit Pythonista Planet to improve your Python skills ")
+newlabel.grid(column=0,row=0)
+window.mainloop()
+
+
+
+#os.system("python C:\Users\alexb\OneDrive\1SchoolOneDrive\ENGT 4050\fast-style-transfer-master\evaluate.py --dir")
