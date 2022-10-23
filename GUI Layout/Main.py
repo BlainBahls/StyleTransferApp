546c765d655af17958fe57265ecc2b0c010d280d
BlainBahls <98792178+BlainBahls@users.noreply.github.com>
2022-10-23 18:59:22 -0400

    Win1, Win2, Main.py abstraction stuff. Had to translates brendans stupid ass word vomit lol

--- GUI Layout/Main.py
+++ GUI Layout/Main.py
@@ -1,65 +1,15 @@
 import sys
+from PyQt5.QtWidgets import QApplication,  QWidget,  QLabel, QMainWindow, QPushButton, QAction
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-
 import cv2
+import Win1
+import Win2
 
-class MainWindow(QWidget):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        #MainWindow.resize(self, 1920,1080)
-
-        self.VBL = QVBoxLayout()
-
-        self.DescLabel = QLabel("Hello World")
-        self.VBL.addWidget(self.DescLabel)
-        
-        self.FeedLabel = QLabel()
-        self.VBL.addWidget(self.FeedLabel)
-
-        #self.CancelBTN = QPushButton("Cancel")
-        #self.CancelBTN.clicked.connect(self.CancelFeed)
-        #self.VBL.addWidget(self.CancelBTN)
-
-        #Can add widget
-
-
-
-        self.Worker1 = Worker1()
-
-        self.Worker1.start()
-        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
-        self.setLayout(self.VBL)
-        self.showMaximized()
-
-
-    def ImageUpdateSlot(self, Image):
-        self.FeedLabel.setPixmap(QPixmap.fromImage(Image))
-
-    def CancelFeed(self):
-        self.Worker1.stop()
-
-class Worker1(QThread):
-    ImageUpdate = pyqtSignal(QImage)
-    def run(self):
-        self.ThreadActive = True
-        Capture = cv2.VideoCapture(0)
-        while self.ThreadActive:
-            ret, frame = Capture.read()
-            if ret:
-                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                FlippedImage = cv2.flip(Image, 1)
-                ConvertToQtFormat = QImage(FlippedImage.data, FlippedImage.shape[1], FlippedImage.shape[0], QImage.Format_RGB888)
-                Pic = ConvertToQtFormat.scaled(1920, 1080)
-                self.ImageUpdate.emit(Pic)
-    def stop(self):
-        self.ThreadActive = False
-        self.quit()
-
-if __name__ == "__main__":
-    App = QApplication(sys.argv)
-    Root = MainWindow()
-    Root.show()
-    sys.exit(App.exec())
+App = QApplication(sys.argv)
+Win1 = Win1.Win1()
+#Win2 = Win2.Win2()
+Win1.show()
+#Win2.show()
+sys.exit(App.exec())
---
+++ GUI Layout/Win1.py
@@ -0,0 +1,54 @@
+import sys
+from PyQt5.QtWidgets import QApplication,  QWidget,  QLabel, QMainWindow, QPushButton, QAction
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import cv2
+
+class Win1(QMainWindow):
+    def __init__(self):
+        super(Win1, self).__init__()
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        lay = QVBoxLayout(central_widget)
+        self.DescLabel = QLabel("Win1")
+        lay.addWidget(self.DescLabel)
+        
+        self.FeedLabel = QLabel()
+        lay.addWidget(self.FeedLabel)
+
+        #self.CancelBTN = QPushButton("Cancel")
+        #self.CancelBTN.clicked.connect(self.CancelFeed)
+        #self.VBL.addWidget(self.CancelBTN)
+
+        self.Worker1 = Worker1()
+
+        self.Worker1.start()
+        self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
+        #self.setLayout(self.VBL)
+        self.showMaximized()
+
+
+    def ImageUpdateSlot(self, Image):
+        self.FeedLabel.setPixmap(QPixmap.fromImage(Image))
+
+    def CancelFeed(self):
+        self.Worker1.stop()
+
+class Worker1(QThread):
+    ImageUpdate = pyqtSignal(QImage)
+    def run(self):
+        self.ThreadActive = True
+        Capture = cv2.VideoCapture(0)
+        while self.ThreadActive:
+            ret, frame = Capture.read()
+            if ret:
+                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                FlippedImage = cv2.flip(Image, 1)
+                ConvertToQtFormat = QImage(FlippedImage.data, FlippedImage.shape[1], FlippedImage.shape[0], QImage.Format_RGB888)
+                Pic = ConvertToQtFormat.scaled(1920, 1080)
+                self.ImageUpdate.emit(Pic)
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
