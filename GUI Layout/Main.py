b8fb2ab3028ed8f91ba8a36bc72b5a8ba79c517e
CyberSurgery <trobert26@rockets.utoledo.edu>
2022-10-27 18:56:22 -0400

    Added time loop in Main.py

--- GUI Layout/Main.py
+++ GUI Layout/Main.py
@@ -6,7 +6,9 @@ from PyQt5.QtCore import *
 import cv2
 import Win1
 import Win2
+import time
 
+pictureTime = 30
 App = QApplication(sys.argv)
 Win1 = Win1.Win1()
 #Win2 = Win2.Win2()
@@ -14,3 +16,9 @@ Win1.show()
 #Win2.show()
 sys.exit(App.exec())
 #suh
+
+for i in range(pictureTime):
+    print("Takes picture -")
+    print("Style the photo -")
+    print("Switch to win2 -")
+time.sleep(1)
