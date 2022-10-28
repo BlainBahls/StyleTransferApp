6288ac1badb09584b16618280a67203706d418df
CyberSurgery <trobert26@rockets.utoledo.edu>
2022-10-27 20:29:17 -0400

    GroupWork Update.

--- GUI Layout/Main.py
+++ GUI Layout/Main.py
@@ -8,17 +8,44 @@ import Win1
 import Win2
 import time
 
-pictureTime = 30
+
+timer=QTimer()
+
 App = QApplication(sys.argv)
 Win1 = Win1.Win1()
-#Win2 = Win2.Win2()
-Win1.show()
+Win2 = Win2.Win2()
+#Win1.show()
 #Win2.show()
+currentWindow = 1
+
+timer.start(5000)
+while True:
+    if timer.remainingTime() == 0:
+        timer.start(5000)
+        if currentWindow == 2:
+            currentWindow = 1
+            Win1.show()
+            Win2.close()
+        if currentWindow == 1:
+            currentWindow = 2
+            Win2.show()
+            Win1.close()
+    #QtCore.QTimer.singleShot(3000, self.clear_win)
+    print(timer.remainingTime())
+
+    
+
+
+def cameraFeed():
+    Win1.show()
+
+def pictureCycle():
+    Win2.show()
+
+
+
+#Win2.show()
+cameraFeed()
+#pictureCycle()
 sys.exit(App.exec())
 #suh
-
-for i in range(pictureTime):
-    print("Takes picture -")
-    print("Style the photo -")
-    print("Switch to win2 -")
-time.sleep(1)
