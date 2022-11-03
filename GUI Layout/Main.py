cbfa264f4aad1b64ec2702f0578ebd37feb77107
CyberSurgery <trobert26@rockets.utoledo.edu>
2022-11-03 19:30:28 -0400

    Working with Timer

--- GUI Layout/Main.py
+++ GUI Layout/Main.py
@@ -7,9 +7,9 @@ import cv2
 import Win1
 import Win2
 import time
+from threading import Timer
 
-
-timer=QTimer()
+#timer=QTimer()
 
 App = QApplication(sys.argv)
 Win1 = Win1.Win1()
@@ -18,20 +18,67 @@ Win2 = Win2.Win2()
 #Win2.show()
 currentWindow = 1
 
-timer.start(5000)
+#timer.start(5000)
+#-----------------------------------------------------------------------------------------|
+def cameraFeed():
+    print(Win1.show())
+
+def pictureCycle():
+    Win2.show()
+# Create a function that will print the time
+def create_countdown_timer(time):
+    print(time,"......")
+
+time_in_sec=5
+
 while True:
-    if timer.remainingTime() == 0:
-        timer.start(5000)
-        if currentWindow == 2:
-            currentWindow = 1
-            Win1.show()
-            Win2.close()
-        if currentWindow == 1:
-            currentWindow = 2
-            Win2.show()
-            Win1.close()
-    #QtCore.QTimer.singleShot(3000, self.clear_win)
-    print(timer.remainingTime())
+    #for times in range(time_in_sec):
+    #    # call the function and pass the current time left
+    #    create_countdown_timer(time_in_sec-times)
+    #    # call the function in every 1 second.
+    #    time.sleep(1)
+  
+    #print("Time is up. Execute Window Activation here: \nPicture is Taken.")
+    #print("Windows 1 ceases and Window 2 is activated to show stylized picture.")
+    #print(cameraFeed())
+
+    #for times in range(time_in_sec):
+    #    # call the function and pass the current time left
+    #    create_countdown_timer(time_in_sec-times)
+    #    # call the function in every 1 second.
+    #    time.sleep(1)
+
+    #print("Time is up. Execute Window Activation here:\nMove back to Live Camera Feed.")
+    #print("Windows 2 ceases and Window 1 is activated.")
+#------------------------------------------------------------------------------------------|    
+    def func1():
+        print("This is window 1.","\n")
+        Win1.show()
+        
+    def func2():
+        print("This is window 2.","\n")
+        Win2.show()
+    App.exec()
+    t1 = Timer(interval=3.0,function=func1)
+    t2 = Timer(interval=6.0,function=func2)
+    
+    t1.start()
+    t2.start()
+    
+
+
+    #if timer.remainingTime() == 0:
+    #    timer.start(5000)
+    #    if currentWindow == 2:
+    #        currentWindow = 1
+    #        Win1.show()
+    #        Win2.close()
+    #    if currentWindow == 1:
+    #        currentWindow = 2
+    #        Win2.show()
+    #        Win1.close()
+    ##QtCore.QTimer.singleShot(3000, self.clear_win)
+    #print(timer.remainingTime())
 
     
 
